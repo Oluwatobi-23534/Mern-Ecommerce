d4e40fd84f793db33cfd13b4993f4ff1f426cc5f
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-12-29 17:09:48 +0100

    Succesfully created the create user api/route

--- rareend/index.js
+++ rareend/index.js
@@ -7,7 +7,8 @@ import cookieParser from 'cookie-parser'
 
 //utils
 import connectDB from './config/db.js'
-import { log } from 'console'
+import userRoutes from './routes/userRoutes.js'
+
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -20,8 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    res.send("New project");
-})
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => console.log(`Server is active on port: ${port}`));
