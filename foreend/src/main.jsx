32b4ef4f6217de032d22a3d67585905ace0946cb
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2024-01-01 16:28:11 +0100

    Successfuly completed user Login functionality

--- foreend/src/main.jsx
+++ foreend/src/main.jsx
@@ -1,4 +1,3 @@
-
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -7,10 +6,14 @@ import { createBrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 
+// Auth
+import Login from "./pages/Auth/Login.jsx";
+
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={ <App/>} />
-  )
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login/> } />
+    </Route>)
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
